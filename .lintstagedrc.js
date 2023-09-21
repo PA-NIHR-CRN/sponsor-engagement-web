5ae1ac72a5c1835f397b10173eab5b42f0a27375
tctc91 <git-commit-address>
2023-09-21 16:24:57 +0100

    fix: tsc

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -6,9 +6,6 @@ const buildPrettierCommand = (filenames) =>
   `npx prettier --write ${filenames.map((f) => path.relative(process.cwd(), f)).join(' ')}`
 
 module.exports = {
-  // Type check TypeScript files
-  // '**/*.(ts|tsx)': () => 'npx tsc --noEmit',
-
   '*.{js,jsx,ts,tsx}': [buildEslintCommand, buildPrettierCommand],
 
   // Prettify only Markdown and JSON files
