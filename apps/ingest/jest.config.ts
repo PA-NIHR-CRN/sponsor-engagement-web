e6bd62afb6c42a854ab0d8a7ea0c615086c6294b
tctc91 <git-commit-address>
2023-09-26 12:43:56 +0100

    fix: ingest coverage report output

--- apps/ingest/jest.config.ts
+++ apps/ingest/jest.config.ts
@@ -7,6 +7,17 @@ const config: Config = {
     '^.+\\.tsx?$': 'ts-jest',
   },
   testPathIgnorePatterns: ['dist', '.vscode'],
+  coverageReporters: ['json-summary', 'text', 'html', 'lcov'],
+  coveragePathIgnorePatterns: ['node_modules'],
+  coverageThreshold: {
+    global: {
+      lines: 96,
+      functions: 100,
+      branches: 87,
+      statements: 97,
+    },
+  },
+  collectCoverageFrom: ['src/**/*.{ts,tsx}'],
 }
 
 module.exports = config
