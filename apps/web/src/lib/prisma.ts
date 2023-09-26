e450151ce60f29a949a66b2cc361d6ef2d5f90a4
dcnihr <dale.christian@nihr.ac.uk>
2023-09-26 13:56:05 +0100

    chore: fix eslint error in import

--- apps/web/src/lib/prisma.ts
+++ apps/web/src/lib/prisma.ts
@@ -1,9 +1,3 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from 'database'
 
 export const prismaClient = new PrismaClient()
-
-// Getting the above via the database package is erroring with "PrismaClient is not a constructor"
-
-// import { PrismaClient } from 'database'
-
-// export const prismaClient = new PrismaClient()
--- packages/eslint-config-custom/library.js
+++ packages/eslint-config-custom/library.js
@@ -23,7 +23,7 @@ module.exports = {
   settings: {
     'import/resolver': {
       typescript: {
-        project,
+        project: ['packages/*/tsconfig.json', 'apps/*/tsconfig.json'],
       },
     },
   },
