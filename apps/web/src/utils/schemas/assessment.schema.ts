e5cbb9e017fcab0462152accf9b3e23d0d4bd20b
tctc91 <git-commit-address>
2023-10-05 16:55:26 +0100

    feat: assess form ui

---
+++ apps/web/src/utils/schemas/assessment.schema.ts
@@ -0,0 +1,35 @@
+import * as z from 'zod'
+import { TEXTAREA_MAX_CHARACTERS } from '../../constants/forms'
+
+export type AssessmentInputs = z.infer<typeof assessmentSchema>
+
+export const assessmentSchema = z
+  .object({
+    status: z.enum(['On track', 'Off track'], {
+      errorMap: () => ({
+        message: 'Select how the study is progressing',
+      }),
+    }),
+
+    furtherInformation: z
+      .string({
+        errorMap: () => ({
+          message: 'Select any additional further information',
+        }),
+      })
+      .array()
+      .nonempty(),
+
+    furtherInformationText: z
+      .string()
+      .optional()
+      .refine((val) => {
+        return (
+          val && val.split(' ').length >= TEXTAREA_MAX_CHARACTERS,
+          {
+            message: `Please provide further information with less than the maximum of ${TEXTAREA_MAX_CHARACTERS} characters`,
+          }
+        )
+      }),
+  })
+  .required()
