54dfc592c9ebce90dc7d49b82f7fa06c4befad2b
tcnihr <tom.christian@nihr.ac.uk>
2023-10-24 11:43:15 +0100

    feat: assessment history in assessment page and study page

--- apps/web/src/utils/date.spec.ts
+++ apps/web/src/utils/date.spec.ts
@@ -14,4 +14,8 @@ describe('formatDate', () => {
   it('returns a correctly formatted date', () => {
     expect(formatDate(dayjs().toISOString())).toEqual('1 January 2001')
   })
+
+  it('returns a correctly formatted date in a shorter format', () => {
+    expect(formatDate(dayjs().toISOString(), 'short')).toEqual('01 Jan 2001')
+  })
 })
