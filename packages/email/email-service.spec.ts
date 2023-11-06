b77d6ccf8f8dae26d35e280f381a54db4f74670d
dcnihr <dale.christian@nihr.ac.uk>
2023-11-06 10:15:28 +0000

    feat: email service

---
+++ packages/email/email-service.spec.ts
@@ -0,0 +1,86 @@
+import * as fs from 'node:fs'
+import path from 'node:path'
+import { SES } from 'aws-sdk'
+import handlebars from 'handlebars'
+import { logger } from 'logger'
+import { EmailService, type EmailArgs } from './email-service'
+
+// Mock dependencies
+jest.mock('aws-sdk')
+jest.mock('node:fs')
+jest.mock('logger')
+
+describe('EmailService', () => {
+  let sesClientMock: jest.Mocked<SES>
+  let emailService: EmailService
+
+  beforeEach(() => {
+    sesClientMock = new SES() as jest.Mocked<SES>
+    emailService = new EmailService(sesClientMock)
+  })
+
+  afterEach(() => {
+    jest.clearAllMocks()
+  })
+
+  it('should send an email with the correct parameters', async () => {
+    const readFileSyncMock = jest.mocked(fs.readFileSync)
+
+    // Mock file system readFileSync
+    readFileSyncMock.mockReturnValueOnce('HTML_CONTENT')
+    readFileSyncMock.mockReturnValueOnce('TEXT_CONTENT')
+
+    // Mock handlebars compile
+    const compileMock = jest.spyOn(handlebars, 'compile')
+    compileMock.mockReturnValueOnce((data) => `COMPILED_HTML(${JSON.stringify(data)})`)
+    compileMock.mockReturnValueOnce((data) => `COMPILED_TEXT(${JSON.stringify(data)})`)
+
+    // Mock SES sendEmail
+    const sendEmailMock = jest.fn().mockResolvedValue({})
+    sesClientMock.sendEmail = jest.fn().mockReturnValueOnce({ promise: sendEmailMock })
+
+    const emailData: EmailArgs = {
+      to: 'recipient@example.com',
+      subject: 'Test Subject',
+      templateName: 'contact-assigned',
+      templateData: { name: 'John Doe' },
+    }
+
+    await emailService.sendEmail(emailData)
+
+    expect(readFileSyncMock).toHaveBeenCalledTimes(2)
+    expect(readFileSyncMock).toHaveBeenNthCalledWith(
+      1,
+      path.resolve(process.cwd(), 'src/templates/emails/contact-assigned.html.hbs'),
+      { encoding: 'utf-8' }
+    )
+    expect(readFileSyncMock).toHaveBeenNthCalledWith(
+      2,
+      path.resolve(process.cwd(), 'src/templates/emails/contact-assigned.text.hbs'),
+      { encoding: 'utf-8' }
+    )
+
+    expect(compileMock).toHaveBeenCalledTimes(2)
+    expect(compileMock).toHaveBeenNthCalledWith(1, 'HTML_CONTENT')
+    expect(compileMock).toHaveBeenNthCalledWith(2, 'TEXT_CONTENT')
+
+    expect(sesClientMock.sendEmail).toHaveBeenCalledTimes(1)
+    expect(sesClientMock.sendEmail).toHaveBeenCalledWith({
+      Source: '"CRNCC Study Assessments" <crncc-study-assessments@nihr.ac.uk>',
+      Destination: { ToAddresses: ['recipient@example.com'] },
+      Message: {
+        Subject: { Data: 'Test Subject', Charset: 'utf-8' },
+        Body: {
+          Text: { Data: 'COMPILED_TEXT({"name":"John Doe"})', Charset: 'utf-8' },
+          Html: { Data: 'COMPILED_HTML({"name":"John Doe"})', Charset: 'utf-8' },
+        },
+      },
+    })
+
+    expect(logger.info).toHaveBeenCalledWith(
+      'Email notification sent to %s with subject %s',
+      'recipient@example.com',
+      'Test Subject'
+    )
+  })
+})
---
+++ packages/email/email-service.ts
@@ -0,0 +1,59 @@
+import * as fs from 'node:fs'
+import path from 'node:path'
+import type { SES } from 'aws-sdk'
+import handlebars from 'handlebars'
+import { logger } from 'logger'
+import { EMAIL_CHARSET, EMAIL_FROM_ADDRESS } from './constants'
+
+handlebars.registerHelper('eq', (a, b) => a === b)
+
+export interface EmailArgs {
+  to: string
+  subject: string
+  templateName: 'contact-assigned' | 'contact-removed' | 'assessment-due'
+  templateData: Record<string, unknown>
+}
+
+export class EmailService {
+  constructor(private sesClient: SES) {}
+
+  sendEmail = async (data: EmailArgs) => {
+    const { subject, to, templateName, templateData } = data
+
+    const htmlSource = fs.readFileSync(path.resolve(process.cwd(), `src/templates/emails/${templateName}.html.hbs`), {
+      encoding: EMAIL_CHARSET,
+    })
+    const textSource = fs.readFileSync(path.resolve(process.cwd(), `src/templates/emails/${templateName}.text.hbs`), {
+      encoding: EMAIL_CHARSET,
+    })
+    const htmlBody = handlebars.compile(htmlSource)(templateData)
+    const textBody = handlebars.compile(textSource)(templateData)
+
+    const message: SES.Types.SendEmailRequest = {
+      Source: `"CRNCC Study Assessments" <${EMAIL_FROM_ADDRESS}>`,
+      Destination: {
+        ToAddresses: [to],
+      },
+      Message: {
+        Subject: {
+          Data: subject,
+          Charset: EMAIL_CHARSET,
+        },
+        Body: {
+          Text: {
+            Data: textBody,
+            Charset: EMAIL_CHARSET,
+          },
+          Html: {
+            Data: htmlBody,
+            Charset: EMAIL_CHARSET,
+          },
+        },
+      },
+    }
+
+    await this.sesClient.sendEmail(message).promise()
+
+    logger.info('Email notification sent to %s with subject %s', to, subject)
+  }
+}
