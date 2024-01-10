e48adc916f8146e885e99d82553c6bdbcb8072a7
dcnihr <dale.christian@nihr.ac.uk>
2024-01-10 19:03:01 +0000

    fix: notify rate limiting issues

--- packages/email/constants.ts
+++ packages/email/constants.ts
@@ -7,8 +7,3 @@ export const EMAIL_CHARSET = 'utf-8'
  * Email from address
  */
 export const EMAIL_FROM_ADDRESS = 'noreply-assessmystudy@nihr.ac.uk'
-
-/**
- * Maximum number of email requests per second allowed by SES
- */
-export const SES_RATE_LIMIT = 14
