d92d9d75c608f069d2723e2f69aa8655d6b8dd29
Chris McNeill <chris.mcneill@paconsulting.com>
2024-01-23 16:56:56 +0000

    chore: removing logging adding db config back

--- packages/qa/hooks/GlobalSetup.ts
+++ packages/qa/hooks/GlobalSetup.ts
@@ -1,15 +1,4 @@
 async function globalSetup() {
   process.env.BASE_URL = 'https://test.assessmystudy.nihr.ac.uk/'
-
-  console.log(process.env.BASE_URL)
-  console.log(process.env.CPMS_TEST_DB_PASSWORD)
-  console.log(process.env.CPMS_TEST_DB_SERVER)
-  console.log(process.env.SE_TEST_DB_PASSWORD)
-  console.log(process.env.SE_TEST_DB_HOST)
-  console.log(process.env.SPONSOR_CONTACT_PASS)
-  console.log(process.env.SPONSOR_CONTACT_MANAGER_PASS)
-  console.log(process.env.CONTACT_MANAGER_PASS)
-  console.log(process.env.SE_NO_LOCAL_ACCOUNT_PASS)
-
 }
 export default globalSetup
