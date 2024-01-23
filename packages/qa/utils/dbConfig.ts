7a5b60d60b2045a56509f855eee06e2e49704478
Chris McNeill <chris.mcneill@paconsulting.com>
2024-01-23 17:03:09 +0000

    chore: removing logging adding db config back

--- packages/qa/utils/dbConfig.ts
+++ packages/qa/utils/dbConfig.ts
@@ -1,15 +1,15 @@
 import mysql, { ConnectionOptions } from 'mysql2'
 
-export const cpmsDbConfig = {
-  user: 'cpmsuser',
-  password: `${process.env.CPMS_TEST_DB_PASSWORD}`, //set in .github/workflows/playwright.yml using GitHub secrets, hardcode when running locally
-  server: `${process.env.CPMS_TEST_DB_SERVER}`, //set in .github/workflows/playwright.yml using GitHub secrets, hardcode when running locally
-  database: 'NIHR.CRN.CPMS.OperationalDatabase',
-  options: {
-    encrypt: false,
-    trustServerCertificate: false,
-  },
-}
+// export const cpmsDbConfig = {
+//   user: 'cpmsuser',
+//   password: `${process.env.CPMS_TEST_DB_PASSWORD}`, //set in .github/workflows/playwright.yml using GitHub secrets, hardcode when running locally
+//   server: `${process.env.CPMS_TEST_DB_SERVER}`, //set in .github/workflows/playwright.yml using GitHub secrets, hardcode when running locally
+//   database: 'NIHR.CRN.CPMS.OperationalDatabase',
+//   options: {
+//     encrypt: false,
+//     trustServerCertificate: false,
+//   },
+// }
 
 const seDbConfig: ConnectionOptions = {
   host: `${process.env.SE_TEST_DB_HOST}`, //set in .github/workflows/playwright.yml using GitHub secrets, hardcode when running locally
