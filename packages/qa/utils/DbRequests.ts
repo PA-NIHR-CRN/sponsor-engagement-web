914e2b2c3f6b962aa87845a06ea171d25199024f
Chris McNeill <chris.mcneill@paconsulting.com>
2024-01-23 14:55:59 +0000

    chore: changing test name and temp removing cpms req to test

--- packages/qa/utils/DbRequests.ts
+++ packages/qa/utils/DbRequests.ts
@@ -2,17 +2,17 @@ import * as sql from 'mssql'
 import { RowDataPacket } from 'mysql2'
 import { seConnection, cpmsDbConfig } from './dbConfig'
 
-export async function cpmsDatabaseReq() {
-  let result
-  try {
-    await sql.connect(cpmsDbConfig)
-    result = await sql.query`SELECT * FROM SysRefStudyStatus`
-  } catch (error) {
-    console.error(error)
-    throw Error('CPMS DB request Failed to Execute correctly, see attached Console Logs')
-  }
-  return result.recordset
-}
+// export async function cpmsDatabaseReq() {
+//   let result
+//   try {
+//     await sql.connect(cpmsDbConfig)
+//     result = await sql.query`SELECT * FROM SysRefStudyStatus`
+//   } catch (error) {
+//     console.error(error)
+//     throw Error('CPMS DB request Failed to Execute correctly, see attached Console Logs')
+//   }
+//   return result.recordset
+// }
 
 export async function seDatabaseReq(query: string): Promise<RowDataPacket[]> {
   return new Promise((resolve, reject) => {
