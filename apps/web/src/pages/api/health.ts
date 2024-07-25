f8cc9f86e3ebac839058db54275031fe62d94013
Joseph Ellis <joseph.ellis@paconsulting.com>
2024-07-25 14:47:57 +0100

    ci: move task def into web app

--- apps/web/src/pages/api/health.ts
+++ apps/web/src/pages/api/health.ts
@@ -1,7 +1,5 @@
-import { logger } from '@nihr-ui/logger'
 import type { NextApiResponse } from 'next'
 
 export default function handler(req, res: NextApiResponse) {
-  logger.info('App healthy')
   res.status(200).send('OK')
 }
