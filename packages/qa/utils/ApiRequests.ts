eeba1cb2fc37e163035041a47754febe4b5d1ef5
Adam Nicolaou-Jones <adam.nicolaou-jones@paconsulting.com>
2024-09-18 10:17:34 +0100

    test: updates api helper to use correct env vars

--- packages/qa/utils/ApiRequests.ts
+++ packages/qa/utils/ApiRequests.ts
@@ -1,13 +1,13 @@
 import axios from 'axios'
 
 export async function getStudyEngagementInfo(studyId: string) {
-  const url = `https://test.cpmsapi.crncc.nihr.ac.uk/api/v1/studies/${studyId}/engagement-info`
+  const url = `${process.env.SE_TEST_API_URL}api/v1/studies/${studyId}/engagement-info`
   const username = process.env.SE_TEST_API_USERNAME
   const password = process.env.SE_TEST_API_PASSWORD
 
   // check if the environment variables are set
-  if (!username || !password) {
-    throw new Error('SE API username or password is not set in environment variables.')
+  if (!username || !password || !url) {
+    throw new Error('SE API username or password or url is not set in environment variables.')
   }
 
   try {
