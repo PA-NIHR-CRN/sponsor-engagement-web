c619eca92ef069613eed44a8c106b51b5a58c963
Adam Nicolaou-Jones <adam.nicolaou-jones@paconsulting.com>
2024-10-02 10:13:47 +0100

    test: db connection refactor

--- packages/qa/utils/DbRequests.ts
+++ packages/qa/utils/DbRequests.ts
@@ -1,9 +1,22 @@
+// import { RowDataPacket } from 'mysql2'
+// import { seConnection } from './dbConfig'
+
+// export async function seDatabaseReq(query: string): Promise<RowDataPacket[]> {
+//   return new Promise((resolve, reject) => {
+//     seConnection.query<RowDataPacket[]>(`${query}`, (err, res) => {
+//       if (err) reject(err)
+//       else resolve(res)
+//     })
+//   })
+// }
+
+// pool refactor:
 import { RowDataPacket } from 'mysql2'
-import { seConnection } from './dbConfig'
+import { seConnectionPool } from './dbConfig'
 
 export async function seDatabaseReq(query: string): Promise<RowDataPacket[]> {
   return new Promise((resolve, reject) => {
-    seConnection.query<RowDataPacket[]>(`${query}`, (err, res) => {
+    seConnectionPool.query<RowDataPacket[]>(`${query}`, (err, res) => {
       if (err) reject(err)
       else resolve(res)
     })
--- packages/qa/utils/dbConfig.ts
+++ packages/qa/utils/dbConfig.ts
@@ -1,5 +1,6 @@
-import mysql, { ConnectionOptions } from 'mysql2'
+// import mysql, { ConnectionOptions } from 'mysql2'
 
+// 1 original code
 // const seDbConfig: ConnectionOptions = {
 //   host: `${process.env.SE_TEST_DB_HOST}`, //set in .github/workflows/playwright.yml using GitHub secrets, hardcode when running locally
 //   user: 'admin',
@@ -9,38 +10,82 @@ import mysql, { ConnectionOptions } from 'mysql2'
 // }
 // export const seConnection = mysql.createConnection(seDbConfig)
 
-const seDbConfig: ConnectionOptions = {
+// 2 error handling refactor:
+// import mysql, { ConnectionOptions } from 'mysql2'
+
+// const seDbConfig: ConnectionOptions = {
+//   host: `${process.env.SE_TEST_DB_HOST}`,
+//   user: 'admin',
+//   password: `${process.env.SE_TEST_DB_PASSWORD}`,
+//   database: 'sponsorengagement',
+//   connectTimeout: 30000,
+//   waitForConnections: true,
+// }
+
+// function createConnection() {
+//   const connection = mysql.createConnection(seDbConfig)
+
+//   connection.connect((err) => {
+//     if (err) {
+//       console.error('Error connecting to the database:', err)
+//       setTimeout(createConnection, 2000)
+//     } else {
+//       console.log('Database connected successfully!')
+//     }
+//   })
+
+//   // handle errors
+//   connection.on('error', (err) => {
+//     console.error('Database connection error:', err)
+//     if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+//       console.log('Connection lost. Reconnecting...')
+//       createConnection() // reconnect
+//     } else {
+//       throw err // rethrow if different error
+//     }
+//   })
+
+//   return connection
+// }
+
+// export const seConnection = createConnection()
+
+// 3 pool refactor:
+import mysql, { PoolOptions } from 'mysql2'
+
+const seDbConfig: PoolOptions = {
   host: `${process.env.SE_TEST_DB_HOST}`,
   user: 'admin',
   password: `${process.env.SE_TEST_DB_PASSWORD}`,
   database: 'sponsorengagement',
-  connectTimeout: 15000,
+  connectTimeout: 30000,
+  waitForConnections: true,
+  connectionLimit: 10,
 }
 
-function createConnection() {
-  const connection = mysql.createConnection(seDbConfig)
-
-  connection.connect((err) => {
-    if (err) {
-      console.error('Error connecting to the database:', err)
-      setTimeout(createConnection, 2000)
-    } else {
-      console.log('Database connected successfully!')
-    }
-  })
+function createPool() {
+  const pool = mysql.createPool(seDbConfig)
 
   // handle errors
-  connection.on('error', (err) => {
-    console.error('Database connection error:', err)
-    if (err.code === 'PROTOCOL_CONNECTION_LOST') {
-      console.log('Connection lost. Reconnecting...')
-      createConnection() // reconnect
-    } else {
-      throw err // rethrow if different error
-    }
+  pool.on('connection', (connection) => {
+    console.log('New database connection established ✔')
+
+    connection.on('error', (err) => {
+      console.error('Database connection error:', err)
+      if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+        console.log('Connection lost! Reconnecting...')
+        createPool() // reconnect if connection lost
+      } else {
+        throw err
+      }
+    })
   })
 
-  return connection
+  pool.on('error', (err) => {
+    console.error('Error in the pool:', err)
+  })
+
+  return pool
 }
 
-export const seConnection = createConnection()
+export const seConnectionPool = createPool()
