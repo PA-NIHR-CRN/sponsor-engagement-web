7ac6ce6ec4392a36f93659a746f62574421af8ce
akadawg <amarpreetkdawgotra@hotmail.com>
2024-10-07 09:46:35 +0100

    feat: update tests

---
+++ packages/shared-utilities/jest.config.ts
@@ -0,0 +1,23 @@
+import type { Config } from 'jest'
+
+const config: Config = {
+  setupFiles: ['<rootDir>/jest.env.js'],
+  setupFilesAfterEnv: ['<rootDir>/src/mocks/prisma.ts'],
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  testPathIgnorePatterns: ['dist', '.vscode'],
+  coverageReporters: ['json-summary', 'text', 'html', 'lcov'],
+  coveragePathIgnorePatterns: ['node_modules'],
+  coverageThreshold: {
+    global: {
+      lines: 96,
+      functions: 100,
+      branches: 85,
+      statements: 97,
+    },
+  },
+  collectCoverageFrom: ['src/**/*.{ts,tsx}'],
+}
+
+module.exports = config
---
+++ packages/shared-utilities/src/lib/setAssessmentDue/setAssessmentDue.spec.ts
@@ -0,0 +1,43 @@
+import { setAssessmentDue } from './index'
+import { prismaMock } from '../../mocks/prisma'
+
+jest.mock('@nihr-ui/logger')
+
+const mockStudyIds = [12, 32, 23]
+
+describe('setAssessmentDue()', () => {
+  it('should update the study `isDueAssessment` flag', async () => {
+    prismaMock.study.updateMany.mockResolvedValueOnce({ count: 1 })
+
+    await setAssessmentDue(mockStudyIds)
+
+    expect(prismaMock.study.updateMany).toHaveBeenCalledTimes(1)
+
+    expect(prismaMock.study.updateMany).toHaveBeenCalledWith({
+      data: {
+        isDueAssessment: true,
+      },
+      where: {
+        id: { in: mockStudyIds },
+        evaluationCategories: {
+          some: { isDeleted: false },
+        },
+        assessments: {
+          every: {
+            createdAt: {
+              lte: expect.any(Date),
+            },
+          },
+        },
+        OR: [
+          { actualOpeningDate: null },
+          {
+            actualOpeningDate: {
+              lte: expect.any(Date),
+            },
+          },
+        ],
+      },
+    })
+  })
+})
