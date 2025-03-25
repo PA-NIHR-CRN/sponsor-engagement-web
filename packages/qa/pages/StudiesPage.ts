5b949ece48e199e598e88aeb026991c1db98f1ad
akadawg <amarpreetkdawgotra@hotmail.com>
2025-03-25 15:46:44 +0000

    feat(test): update due test logic

--- packages/qa/pages/StudiesPage.ts
+++ packages/qa/pages/StudiesPage.ts
@@ -381,7 +381,7 @@ export default class StudiesPage {
   async assertListEndsWithNonDueStudies(sortedList: RowDataPacket[]) {
     function checkForStudyNotDue(studies: any) {
       for (let study of studies) {
-        if (Boolean(study.isDueAssessment)) {
+        if (Boolean(study.dueAssessmentAt)) {
           console.log('At least 1 study is not due an assessment, checking last study status...')
           return true
         }
