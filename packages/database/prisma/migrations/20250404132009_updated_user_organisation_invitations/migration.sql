a9fd29964ca0127d7ae57b6d96c5d0542e38404d
akadawg <amarpreetkdawgotra@hotmail.com>
2025-04-10 12:13:10 +0100

    feat: add default value for new required field

--- packages/database/prisma/migrations/20250404132009_updated_user_organisation_invitations/migration.sql
+++ packages/database/prisma/migrations/20250404132009_updated_user_organisation_invitations/migration.sql
@@ -1,12 +1,20 @@
-/*
-  Warnings:
-
-  - Added the required column `sentById` to the `UserOrganisationInvitation` table without a default value. This is not possible if the table is not empty.
-
-*/
 -- AlterTable
 ALTER TABLE `UserOrganisationInvitation` ADD COLUMN `isDeleted` BOOLEAN NULL DEFAULT false,
-    ADD COLUMN `sentById` INTEGER NOT NULL;
+    ADD COLUMN `sentById` INTEGER  NULL;
+
+
+-- Set `sentById` field to the 'Adminstrator' User id
+UPDATE `UserOrganisationInvitation` userOrgInvitation 
+JOIN  (
+  SELECT `id`
+  FROM `User`
+  WHERE `email` = 'administrator@nihr.ac.uk'
+  LIMIT 1
+) AS adminUser
+SET userOrgInvitation.`sentById` = adminUser.`id`;
+
+--- Update `sentById` field to be required
+ALTER TABLE `UserOrganisationInvitation` MODIFY COLUMN `sentById` INT NOT NULL;
 
 -- AddForeignKey
 ALTER TABLE `UserOrganisationInvitation` ADD CONSTRAINT `UserOrganisationInvitation_sentById_fkey` FOREIGN KEY (`sentById`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
