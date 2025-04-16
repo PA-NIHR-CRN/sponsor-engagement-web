599252ef5bf60870cb4536046a05acf10f2cd286
akadawg <amarpreetkdawgotra@hotmail.com>
2025-04-16 13:30:37 +0100

    feat: update retry logic and modify task scheduling time

--- apps/invitation-monitor/src/lib/constants.ts
+++ apps/invitation-monitor/src/lib/constants.ts
@@ -7,3 +7,16 @@ import { EventType } from '@aws-sdk/client-sesv2'
 export const UserOrganisationInviteStatus = { SUCCESS: 'Success', FAILURE: 'Failure', PENDING: 'Pending' }
 
 export const PERMANENT_EMAIL_FAILURES: string[] = [EventType.RENDERING_FAILURE, EventType.REJECT]
+
+/**
+ * A list of Amazon SES errors that we want to attempt a retry after receiving
+ */
+export const RETRYABLE_SES_ERRORS = [
+  'InternalFailure',
+  'RequestAbortedException',
+  'RequestExpired',
+  'RequestTimeoutException',
+  'ServiceUnavailable',
+  'ThrottlingException',
+  'TooManyRequestsException',
+]
