1e663b5ac7844818fdb99529d83afaa78a2f39c8
iimaanm <145764267+iimaanm@users.noreply.github.com>
2025-05-06 14:48:01 +0100

    fix: updating tests

--- packages/qa/tests/features/organisationDetailsTests/failedToDeliverTagTest.e2e.ts
+++ packages/qa/tests/features/organisationDetailsTests/failedToDeliverTagTest.e2e.ts
@@ -2,36 +2,26 @@ import { test } from '../../../hooks/CustomFixtures'
 import { seDatabaseReq } from '../../../utils/DbRequests'
 import { RowDataPacket } from 'mysql2'
 
-let expectedResponse: RowDataPacket[]
-let randomOrganisationId: RowDataPacket[]
+let response: RowDataPacket[]
 
 test.beforeAll('Setup Test', async () => {
-  const randomOrganisationIdResponse = await seDatabaseReq(`
-     SELECT organisationId FROM UserOrganisationInvitation
-     JOIN UserOrganisation ON UserOrganisationInvitation.userOrganisationId = UserOrganisation.id
-     WHERE UserOrganisationInvitation.isDeleted = 0 AND UserOrganisation.isDeleted = 0
-     ORDER BY RAND()
-     LIMIT 1;`)
-
-  if (!randomOrganisationIdResponse) {
-    throw new Error('No organisationId was returned from the database')
+  response = await seDatabaseReq(`
+    SELECT UserOrganisation.organisationId, User.email, UserOrganisationInvitation.statusId FROM UserOrganisationInvitation
+    JOIN UserOrganisation ON UserOrganisation.id = UserOrganisationInvitation.userOrganisationId
+    JOIN User ON User.id = UserOrganisation.userId
+    WHERE UserOrganisationInvitation.isDeleted = 0 AND UserOrganisation.isDeleted = 0
+    AND UserOrganisation.organisationId = (select uo.organisationId FROM UserOrganisationInvitation uoi
+    JOIN UserOrganisation uo ON uo.id = uoi.userOrganisationId
+    JOIN Organisation o ON o.id = uo.organisationId
+    WHERE uoi.statusId = 3 AND uoi.isDeleted = 0 AND uo.isDeleted = 0
+    GROUP BY organisationId LIMIT 1)
+    ORDER BY UserOrganisationInvitation.timestamp DESC;
+  `)
+  if (response.length === 0) {
+    throw new Error(
+      'No invitations emails returned from the database for an organisation with at least one failed invite email.'
+    )
   }
-  randomOrganisationId = randomOrganisationIdResponse[0]?.organisationId
-
-  const response = await seDatabaseReq(`
-    SELECT UserOrganisation.organisationId, UserOrganisationInvitation.statusId, UserOrganisation.userId, User.email
-    FROM UserOrganisationInvitation
-    JOIN UserOrganisation ON UserOrganisationInvitation.userOrganisationId = UserOrganisation.id
-    JOIN User ON UserOrganisation.userId = User.id
-    WHERE UserOrganisation.isDeleted = 0 
-    AND UserOrganisationInvitation.isDeleted = 0 
-    AND UserOrganisation.organisationId = ${randomOrganisationId}
-    ORDER BY UserOrganisationInvitation.timestamp`)
-
-  if (!response || response.length === 0) {
-    throw new Error('No users returned from the database.')
-  }
-  expectedResponse = response
 })
 
 test.describe('Failed to deliver email tag validation for Organisation Details Page - @se_251', () => {
@@ -39,23 +29,17 @@ test.describe('Failed to deliver email tag validation for Organisation Details P
   test('As a Contact Manager I can see a failed to deliver tag when a sponsor contact invite has not been sent successfully on the Orgnisation Details Page - @se_251_ac1', async ({
     organisationDetailsPage,
   }) => {
-    await test.step('Given I have navigated to the Organisation Details Page', async () => {
-      await organisationDetailsPage.goto(expectedResponse[0].organisationId.toString())
+    await test.step('Given I have navigated to the organisation Details Page', async () => {
+      await organisationDetailsPage.goto(response[0].organisationId.toString())
     })
     await test.step('Then I am taken to the organisation details page', async () => {
-      await organisationDetailsPage.assertOnOrganisationDetailsPage(expectedResponse[0].organisationId.toString())
+      await organisationDetailsPage.assertOnOrganisationDetailsPage(response[0].organisationId.toString())
     })
-    await test.step('And it will contain a List of Contacts for that Organisation', async () => {
-      await organisationDetailsPage.assertContactListDisplayed(true)
-    })
-    await test.step('And it will contain a List of Contacts for that Organisation', async () => {
+    await test.step('And it will contain a List of Contacts for that organisation', async () => {
       await organisationDetailsPage.assertContactListDisplayed(true)
     })
     await test.step('And each contact that has not received an invite email will have a Failed to deliver email tag', async () => {
-      await organisationDetailsPage.assertContactFailedToDeliverTag(
-        expectedResponse[0].email,
-        expectedResponse[0].statusId
-      )
+      await organisationDetailsPage.assertContactFailedToDeliverTag(response)
     })
   })
 })
