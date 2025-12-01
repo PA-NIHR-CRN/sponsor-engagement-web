3f92464c6d24ff4998b2c862e290b45f1f8faf15
kevinofficer <57766007+kevinofficer@users.noreply.github.com>
2025-12-01 11:49:28 +0000

    feat: update crn url too

--- apps/notify/src/constants.ts
+++ apps/notify/src/constants.ts
@@ -6,7 +6,8 @@ export const SIGN_IN_PAGE = '/auth/signin'
 /**
  * External URLs
  */
-export const EXTERNAL_CRN_URL = 'https://www.nihr.ac.uk/explore-nihr/support/clinical-research-network.htm'
+export const EXTERNAL_CRN_URL =
+  'https://www.nihr.ac.uk/support-and-services/support-for-delivering-research/research-delivery-network'
 
 export const EXTERNAL_CRN_TERMS_CONDITIONS_URL =
   'https://www.nihr.ac.uk/eligibility-nihr-research-delivery-network-support#portfolio-terms-and-conditions'
